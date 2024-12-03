1af7da838bcdf058cf2adffee4d7296bf20e10dc
paolodeidda <paolodeidda.97@gmail.com>
2024-12-03 15:21:27 +0100

    added button to create new household

--- app/src/main/java/com/example/inventorymapper/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/inventorymapper/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -26,14 +27,23 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        // Initialize RecyclerView
         recyclerView = root.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         adapter = new HouseholdAdapter(new ArrayList<>());
         recyclerView.setAdapter(adapter);
 
+        // Add click listener to "Add new location" TextView
+        TextView addNewText = root.findViewById(R.id.add_new_text);
+        addNewText.setOnClickListener(v -> {
+            // Show the form to create a new household
+            HouseholdCreationForm form = new HouseholdCreationForm();
+            form.show(getParentFragmentManager(), "Household-form");
+        });
+
+        // Observe data from ViewModel and update RecyclerView
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         homeViewModel.getHouseholds().observe(getViewLifecycleOwner(), households -> {
-            Log.d("asdf", "asdf");
             adapter.setHouseholds(households);
         });
 
