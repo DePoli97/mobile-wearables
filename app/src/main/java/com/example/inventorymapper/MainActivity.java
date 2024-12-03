3c9caad5a5633d2a7e50842d38dd3aa69196f7eb
paolodeidda <paolodeidda.97@gmail.com>
2024-12-03 16:39:34 +0100

    changed icons

--- app/src/main/java/com/example/inventorymapper/MainActivity.java
+++ app/src/main/java/com/example/inventorymapper/MainActivity.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.view.Menu;
 
 import com.example.inventorymapper.ui.home.HouseholdCreationForm;
-import com.example.inventorymapper.ui.home.ItemCreationForm;
 import com.example.inventorymapper.ui.home.LocationCreationForm;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
@@ -51,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_households, R.id.nav_search, R.id.nav_user)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
