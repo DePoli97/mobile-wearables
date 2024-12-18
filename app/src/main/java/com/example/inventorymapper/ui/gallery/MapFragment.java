6a75c3b26527f570a3145fc6441c7aa2306f07a0
Nick Mazzu <mazzun@usi.ch>
2024-12-18 09:42:21 +0100

    added tooltip

--- app/src/main/java/com/example/inventorymapper/ui/gallery/MapFragment.java
+++ app/src/main/java/com/example/inventorymapper/ui/gallery/MapFragment.java
@@ -7,8 +7,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -25,6 +27,7 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.infowindow.InfoWindow;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -58,8 +61,42 @@ public class MapFragment extends Fragment {
                 markerList = households.stream().map(household -> {
                     Marker marker = new Marker(map);
                     marker.setIcon(ContextCompat.getDrawable(getContext(), R.drawable.mapic_house_window));
-                    marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                    marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM + (float) 0.1);
                     marker.setPosition(new GeoPoint(household.getLatitude(), household.getLongitude()));
+
+                    InfoWindow info = new InfoWindow(R.layout.map_house_tooltip, map) {
+                        @Override
+                        public void onOpen(Object item) {
+                            Household _household = (Household) ((Marker) item).getRelatedObject();
+                            View root = getView();
+                            root.setOnClickListener(v -> {
+                                close();
+                            });
+                            CardView card = root.findViewById(R.id.card);
+                            card.setOnClickListener(v -> {
+                                close();
+                            });
+                            TextView name = root.findViewById(R.id.house_name);
+                            name.setOnClickListener(v -> {
+                                close();
+                            });
+                            TextView desc = root.findViewById(R.id.house_desc);
+                            desc.setOnClickListener(v -> {
+                                close();
+                            });
+                            name.setText(_household.getName());
+                            desc.setText(_household.getLocation().getDescription());
+                            Log.d("Map", "opened");
+                        }
+
+                        @Override
+                        public void onClose() {
+                        }
+                    };
+
+                    info.setRelatedObject(household);
+                    marker.setRelatedObject(household);
+                    marker.setInfoWindow(info);
                     Log.d("Map", String.format("Adding household at %f - %f", household.getLatitude(), household.getLongitude()));
                     return marker;
                 }).collect(Collectors.toList());
@@ -69,7 +106,7 @@ public class MapFragment extends Fragment {
         });
 
         userMarker = new Marker(map);
-        userMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        userMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_TOP);
         userMarker.setIcon(ContextCompat.getDrawable(getContext(), R.drawable.mapic_person));
         userMarker.setPosition(currentPos);
         map.getOverlays().add(userMarker);
